564890bd2c786c63ecf213601a6dbd5dd1321ad7
prayugo <pray.arsian24@gmai.com>
2022-10-22 06:38:52 +0700

    penggunaan state

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,45 @@
 import React from "react";
-import Header from "./Header";
-import List from "./List";
+// import Header from "./Header";
+// import List from "./List";
 
 
 // jsx adalah javascript xml, extensi javascript yang memungkinkan untuk menulis html falam javascript
 // aturan menggunakan jsx hanya boleh menggunakan 1 root tag
 class App extends React.Component{
+    state = {
+        book : {
+            price : 200000
+        },
+        detail : [
+            "Description text",
+            "have much price"
+        ],
+        name: "whose child is this",
+        author: "Bil wilshon",
+    }
+
+
     render(){
         return(
             // root component
             <div>
                 {/* parent component */}
-                    <Header/>
-                    <List/>
+                    {/* <Header/>
+                    <List/> */}
                 {/* parent component */}
+
+
                 <h1>Hello Word</h1> 
-                <p>test</p>
+                {/* ini state */}
+                <h3>Judul : { this.state.name }</h3>
+                <p>author : { this.state.author }</p>
+                <p>Price : { this.state.book.price }</p>
+                { this.state.detail.map (item => 
+                    <div>
+                        <h3> {item} </h3>
+                    </div>
+                ) }
+                {/* ini state */}
             </div>
             // root component
 
@@ -35,3 +59,5 @@ class App extends React.Component{
 // }
 
 export default App;
+
+// state data yang bakal kita tampilin dalam component
