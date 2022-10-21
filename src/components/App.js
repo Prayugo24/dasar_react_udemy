2875736be4b3c1b6af2a4c62c98d4ac7386852f1
prayugo <pray.arsian24@gmai.com>
2022-10-22 05:53:08 +0700

    penjelasan jsx

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-
+// jsx adalah javascript xml, extensi javascript yang memungkinkan untuk menulis html falam javascript
+// aturan menggunakan jsx hanya boleh menggunakan 1 root tag
 class App extends React.Component{
     render(){
         return(
             <div>
                 <h1>Hello Word</h1> 
+                <p>test</p>
             </div>
+            // <div></div>  tag ini lebih dari 1 tidak boleh dalam jsx
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import  ReactDOM  from "react-dom"
 import App from "./components/App"
 
+
+
 ReactDOM.render(
     // untuk memberikan warning pada react jika ada code / fitur yang sudah tidak di support lagi React stricmode
     <React.StrictMode> 
