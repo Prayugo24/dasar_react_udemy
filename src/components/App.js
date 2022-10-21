6399f61b551b5a828c3998f857ceb89170aeb3ab
prayugo <pray.arsian24@gmai.com>
2022-10-22 05:57:51 +0700

    function component

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,25 @@ import React from "react";
 
 // jsx adalah javascript xml, extensi javascript yang memungkinkan untuk menulis html falam javascript
 // aturan menggunakan jsx hanya boleh menggunakan 1 root tag
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>Hello Word</h1> 
-                <p>test</p>
-            </div>
-            // <div></div>  tag ini lebih dari 1 tidak boleh dalam jsx
-        )
-    }
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Hello Word</h1> 
+//                 <p>test</p>
+//             </div>
+//             // <div></div>  tag ini lebih dari 1 tidak boleh dalam jsx
+//         )
+//     }
+// }
+
+// function component
+const App = () => {
+    return(
+        <div>
+            <h1>Hello Word</h1>
+        </div>
+    )
 }
 
 export default App;
