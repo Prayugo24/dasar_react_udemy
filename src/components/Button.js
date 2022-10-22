1001f1334cedc2135884ed0d9638207310b94bf8
prayugo <pray.arsian24@gmai.com>
2022-10-22 07:53:20 +0700

    penggunaan set state

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,19 @@ import React from "react";
 import PropTypes from "prop-types"
 
 class Button extends React.Component{
-    // cara penggunaan event pada button
+    state = {
+        name:"kodeakademia"
+    }
+    // cara penggunaan event pada button, dan penggunaan set state
     change = () =>{
-        console.log("Ok")
+        this.setState({
+            name:"Herry poter"
+        })
     }
     render(){
         return(
             <div>
+                <h1>{this.state.name}</h1>
                 <button onClick={this.change}>Click My App</button>
             </div>
         )
@@ -24,5 +30,7 @@ Button.propTypes = {
 Button.defaultProps = {
     color:"red"
 }
+
+// state set function dalam react yang digunakan untuk mengubah state di react kita tidak boleh mengibah state secara langsung
 
 export default Button;
