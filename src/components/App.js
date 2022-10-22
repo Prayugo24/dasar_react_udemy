da95b457aa97e2a67e389174fa3b314a0329db79
prayugo <pray.arsian24@gmai.com>
2022-10-22 08:31:32 +0700

    penggunaan css react

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 // import Header from "./Header";
 // import List from "./List";
-import Button from "./Button";
+// import Button from "./Button";
+import "../style/App.css"
 
 
 // jsx adalah javascript xml, extensi javascript yang memungkinkan untuk menulis html falam javascript
 // aturan menggunakan jsx hanya boleh menggunakan 1 root tag
+
+// styling component di react
+// 1. inline style
+// 2. external CSS
+// 3. css preprocessor
+// 4. library : syled component
+
 class App extends React.Component{
     state = {
         book : {
@@ -22,8 +30,14 @@ class App extends React.Component{
     render(){
         return(
             // root component
-            <div>
-                <Button color="biru"/>
+            // penggunaan css inline
+            // <div style={{ width:"100px", backgroundColor:"red"}}>
+
+            // penggunaan css external
+            <div className="box" >
+                <h1 style={myStyle}>Hello</h1>
+
+                {/* <Button color="biru"/> */}
 
                 {/* parent component */}
                     {/* <Header/> */}
@@ -59,6 +73,11 @@ class App extends React.Component{
 //         </div>
 //     )
 // }
+
+const myStyle = {
+    color:"blue",
+    fontSize: "50px"
+}
 
 export default App;
 
